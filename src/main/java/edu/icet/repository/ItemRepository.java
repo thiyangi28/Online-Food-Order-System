642221adada64caadb0f98dd2ad6575e32d7ee26
thiyangi28 <thiyangip24@gmail.com>
2025-11-02 13:43:52 +0530

    Item Class implementation

--- src/main/java/edu/icet/repository/ItemRepository.java
+++ src/main/java/edu/icet/repository/ItemRepository.java
@@ -2,7 +2,9 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<ItemEntity,String> {
 
 }
--- src/main/java/edu/icet/service/ItemService.java
+++ src/main/java/edu/icet/service/ItemService.java
@@ -1,20 +1,57 @@
 package edu.icet.service;
 
 import edu.icet.model.dto.Item;
+import edu.icet.model.entity.ItemEntity;
 
+import edu.icet.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
     public List<Item> getAllDetails() {
+        List<ItemEntity> all = itemRepository.findAll();
+        List<Item> items = new ArrayList<>();
 
+        for (ItemEntity itemEntity:all){
+            items.add(new Item(
+                    itemEntity.getItemCode(),
+                    itemEntity.getDescription(),
+                    itemEntity.getPackSize(),
+                    itemEntity.getUnitPrice(),
+                    itemEntity.getQuantity()
+                    ));
+        }
+        return items;
     }
 
-    public void add(Item item) {
+    public void add(Item addItem) {
+        itemRepository.save(new ItemEntity(
+                addItem.getItemCode(),
+                addItem.getDescription(),
+                addItem.getPackSize(),
+                addItem.getUnitPrice(),
+                addItem.getQuantity()
+        ));
     }
 
-    public void update(Item item) {
+    public void update(Item updateItem) {
+        itemRepository.save(new ItemEntity(
+                updateItem.getItemCode(),
+                updateItem.getDescription(),
+                updateItem.getPackSize(),
+                updateItem.getUnitPrice(),
+                updateItem.getQuantity()
+        ));
     }
 
     public void deleteId(String id) {
+        itemRepository.deleteById(id);
     }
 }
